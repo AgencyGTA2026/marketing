bbb6d301680223337f843c71fd9aaddc8b4f70f6
Connor Fitzgerald <connorfitzgerald17@gmail.com>
2026-06-23 16:46:26 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,7 @@ export default function RootLayout({
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
                 gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_AD_ID}');
               `}
             </Script>
           </>
